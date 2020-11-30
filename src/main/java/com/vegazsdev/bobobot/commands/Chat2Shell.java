765a608c23292ec7478c880bb186587268c3deff
Luoer | 罗尔 <66991791+qrovic@users.noreply.github.com>
2020-12-01 00:22:02 +0800

    Make live output. Thanks @yukosky for the hint

--- src/main/java/com/vegazsdev/bobobot/commands/Chat2Shell.java
+++ src/main/java/com/vegazsdev/bobobot/commands/Chat2Shell.java
@@ -18,26 +18,29 @@ public class Chat2Shell extends Command {
     private static final Logger LOGGER = (Logger) LogManager.getLogger(Chat2Shell.class);
 
     public Chat2Shell() {
-        super("shell", "Run shell (bash) commands via chat");
+        super("shell", "Run shell (bash) commands via chat (Only Master can run)");
     }
 
     @Override
     public void botReply(Update update, TelegramBot bot, PrefObj prefs) {
         if (update.getMessage().getFrom().getId() == Float.parseFloat(Objects.requireNonNull(Config.getDefConfig("bot-master")))) {
             String msg = update.getMessage().getText().substring(7);
+            ProcessBuilder pb;
+            pb = new ProcessBuilder("/bin/bash", "-c", msg);
+            StringBuilder fullLogs = new StringBuilder();
+            fullLogs.append("***$ " + msg + "***\n");
+            int id = bot.sendReply(fullLogs.toString(), update);
             try {
-                StringBuilder text = new StringBuilder();
-                ProcessBuilder pb;
-                pb = new ProcessBuilder("/bin/bash", "-c", msg);
                 pb.redirectErrorStream(true);
                 Process process = pb.start();
                 InputStream is = process.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(is));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    text.append(line).append("\n");
+                    fullLogs.append("`").append(line).append("`").append("\n");
+                    bot.editMessage(fullLogs.toString(), update, id);
                 }
-                bot.sendMessage("`" + text.toString() + "`", update);
+                bot.editMessage(fullLogs.toString(), update, id);
             } catch (Exception e) {
                 bot.sendMessage(prefs.getString("something_went_wrong"), update);
                 LOGGER.error(e.getMessage(), e);
